72bfa16a86e0dabcc5edd92035634727005d5940
Franciele-Santos-Silva <franciellysantos876@gmail.com>
2025-08-05 01:28:14 -0300

    Mova os arquivos do projeto para dentro da pasta backend

---
+++ backend/src/controllers/tasksController.js
@@ -0,0 +1,13 @@
+// salva as funcçoes de dentro do router
+const tasksModel = require('../models/tasksModel');
+
+const  getAll = async(req, res) => {
+
+    const tasks = await tasksModel.getAll();
+
+    return res.status(200).json(tasks);
+};
+
+module.exports = {
+    getAll
+}; 
