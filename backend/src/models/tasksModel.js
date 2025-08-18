8209622d18c7118147269b018621fadcc3f4f511
Franciele-Santos-Silva <franciellysantos876@gmail.com>
2025-08-18 16:12:52 -0300

    Erro em taskModel

--- backend/src/models/tasksModel.js
+++ backend/src/models/tasksModel.js
@@ -9,11 +9,10 @@ const createTask = async (task) => {
      const {title} = task;
     const dateUTC = new Date(Date.now()).toUTCString();
 
-    const query = ('INSET INTO tasks(title, status, created_at) VALUES(?,?,?)');
+    const query = ('INSERT INTO tasks(title, status, create_at) VALUES(?,?,?)');
 
-    const createTask = await connection.execute(query, [title, 'pendente', dateUTC ]);
-
-    return createTask;
+    const createdTask = await connection.execute(query, [title, 'pendente', dateUTC ]);
+    return {insertId: createdTask.insertId};
 }
 
 module.exports = {
