eb9ffa5723c877c3abd57c80b4244e6d8384bbb5
Franciele-Santos-Silva <franciellysantos876@gmail.com>
2025-09-18 23:56:33 -0300

    Erro no Controller arrumado

--- backend/src/router.js
+++ backend/src/router.js
@@ -1,15 +1,3 @@
-// const express = require('express');
-// const tasksController = require('./controllers/tasksController');
-// const { validateBody } = require('./middlewares/tasksMiddleware');
-
-// const router = express.Router();
-
-// router.get('/tasks', tasksController.getAll);
-// router.post('/tasks', validateBody, tasksController.createTask);
-// router.delete('/tasks/:id', tasksController.deleteTask);
-
-// module.exports = router;
-
 const express = require('express');
 const tasksController = require('./controllers/tasksController');
 const { validateBody } = require('./middlewares/tasksMiddleware');
