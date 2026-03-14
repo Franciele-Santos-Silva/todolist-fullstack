e3dfdcf50a7db4efc367af76fb2ba99dadf9a45c
Franciele Santos Silva <franciellysantos876@gmail.com>
2026-03-14 08:58:52 -0300

    fix(frontend): corrigir erro no estilo e conexão com backend

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,16 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
       "/tasks": {
-        target: "http://backend:3333", // aqui usa o nome do container backend
-        changeOrigin: true,
-        secure: false,
-      },
-    },
-  },
+        target: process.env.VITE_API_URL || "http://localhost:3333",
+        changeOrigin: true
+      }
+    }
+  }
 });
